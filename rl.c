4542382b008d55eca9e3f18c3715f04b76ffa960
L8D <tenorbiel@gmail.com>
2013-11-23 17:47:04 -0600

    added several more operators including and if conditional

--- rl.c
+++ rl.c
@@ -6,10 +6,12 @@
 #include "stack.c"
 
 #define WHITESPACE " \t\n\v\f\r"
-#define OPER(op) n = pop(st); push(st, pop(st) op n); return;
+#define OPER(op) n = pop(st); push(st, pop(st) op n); return
+#define BOOLOPER(op) n = pop(st); push(st, pop(st) op n ? -1 : 0); return
 
 void interpret(char *word, stack *st) {
     float n = 0;
+    char *r;
 
     sscanf(word, "%g", &n);
     if(n != 0 || *word == '0') {
@@ -17,18 +19,45 @@ void interpret(char *word, stack *st) {
         return;
     }
 
+    if(word[1] == 0)
     switch(*word) {
-        case '+': OPER(+)
-        case '-': OPER(-)
-        case '*': OPER(*)
-        case '/': OPER(/)
+        case '+': OPER(+);
+        case '-': OPER(-);
+        case '*': OPER(*);
+        case '/': OPER(/);
+
+        case '>': BOOLOPER(>);
+        case '<': BOOLOPER(<);
+        case '!': push(st, !pop(st) ? -1 : 0);
+        case '=': BOOLOPER(==);
+
+        case '_':
+            n = pop(st);
+            push(st, n); push(st, n);
+            return;
+
+        case ',':
+            pop(st);
+            return;
 
         case '.':
             printf("%g\n", pop(st));
             return;
 
-        default:
-            push(st, 0);
+        case '?':
+            n = pop(st);
+            r = strtok(NULL, WHITESPACE);
+            while(r != NULL) {
+                if(*r == ':' && r[1] == 0) break;
+                if(n) interpret(r, st);
+                r = strtok(NULL, WHITESPACE);
+            }
+
+            while(r != NULL) {
+                if(*r == ';' && r[1] == 0) break;
+                if(!n) interpret(r, st);
+                r = strtok(NULL, WHITESPACE);
+            }
             return;
     }
 }
